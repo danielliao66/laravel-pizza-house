07051c6400fba0f37cb793f67404cab0968e3067
danielliao66 <danielliao66@gmail.com>
2021-10-15 19:36:51 +0800

    add something

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pizzas', function () {
-    $pizzas = [
-        ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-        ['type' => 'volcano', 'base' => 'garlic crust'],
-        ['type' => 'veg supreme', 'base' => 'thin & crispy']
-    ];
-    return view('pizzas', [
-        'pizzas' => $pizzas,
-        'name' => request('name'),
-        'age' => request('age')
-    ]);
-});
-
-Route::get('/pizzas/{id}', function ($id) {
-    return view('details', ['id' => $id]);
-});
+Route::get('/pizzas', 'PizzaController@index');
+Route::get('/pizzas/create', 'PizzaController@create');
+Route::post('/pizzas', 'PizzaController@store');
+Route::get('/pizzas/{id}', 'PizzaController@show');
